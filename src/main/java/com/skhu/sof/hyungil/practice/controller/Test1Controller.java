bba0eeffaaff301d224c06e4c2a9e7b3343ff3b8
Hyung1Jung <hyungiljung94@gmail.com>
2020-10-20 15:31:03 +0900

    record

---
+++ src/main/java/com/skhu/sof/hyungil/practice/controller/Test1Controller.java
@@ -0,0 +1,7 @@
+package com.skhu.sof.hyungil.practice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test1Controller {
+}
---
+++ src/test/java/com/skhu/sof/hyungil/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhu.sof.hyungil.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
