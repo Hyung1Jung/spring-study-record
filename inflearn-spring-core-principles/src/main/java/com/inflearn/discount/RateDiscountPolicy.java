772d71d5417ee6dd1fa2e05a894d01bac6b738ce
Hyung1Jung <junghyungile@gmail.com>
2021-01-26 17:10:51 +0900

    feat : 정률 할인 추가(순수 자바로만) -> 프로덕션 코드

---
+++ inflearn-spring-core-principles/src/main/java/com/inflearn/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package com.inflearn.discount;
+
+import com.inflearn.member.Grade;
+import com.inflearn.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) { // ctrl + shift + t -> 테스트 추가 단축키
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
