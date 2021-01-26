770f554df5418c17618a0e5eba156fb1e142e25d
Hyung1Jung <junghyungile@gmail.com>
2021-01-26 14:41:23 +0900

    feat : 순수 자바로만 회원가입 -> 테스트 코드 추가

---
+++ inflearn-spring-core-principles/src/test/java/com/inflearn/member/MemberServiceTest.java
@@ -0,0 +1,23 @@
+package com.inflearn.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceimpl();
+
+    @Test
+    void join() {
+        // given -> ~ 환경이 주어졌을 때,
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when -> 이렇게 했을 때,
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        // then -> 이렇게 된다.
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
