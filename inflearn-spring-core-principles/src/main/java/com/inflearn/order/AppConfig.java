dd249bcbc133d50640c45e1f0550e3e915ff692c
Hyung1Jung <junghyungile@gmail.com>
2021-01-27 15:54:59 +0900

    refactor(AppConfig) : 중복제거

--- inflearn-spring-core-principles/src/main/java/com/inflearn/order/AppConfig.java
+++ inflearn-spring-core-principles/src/main/java/com/inflearn/order/AppConfig.java
@@ -1,6 +1,8 @@
 package com.inflearn.order;
 
+import com.inflearn.discount.DiscountPolicy;
 import com.inflearn.discount.FixDiscountPolicy;
+import com.inflearn.member.MemberRepository;
 import com.inflearn.member.MemberService;
 import com.inflearn.member.MemberServiceimpl;
 import com.inflearn.member.MemoryMemberRepository;
@@ -8,10 +10,18 @@ import com.inflearn.member.MemoryMemberRepository;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceimpl(new MemoryMemberRepository());
+        return new MemberServiceimpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceimpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceimpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
