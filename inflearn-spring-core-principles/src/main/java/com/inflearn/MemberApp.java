a3e200e8db0fe8b2057da6a21f75e01caaf843d4
Hyung1Jung <junghyungile@gmail.com>
2021-01-28 14:27:13 +0900

    refactor : bean 등록 후 스프링 컨테이너로 관리

--- inflearn-spring-core-principles/src/main/java/com/inflearn/MemberApp.java
+++ inflearn-spring-core-principles/src/main/java/com/inflearn/MemberApp.java
@@ -4,12 +4,19 @@ import com.inflearn.member.Grade;
 import com.inflearn.member.Member;
 import com.inflearn.member.MemberService;
 import com.inflearn.order.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+//      ApplicationContext가 컨테이너라고 보면 되고 applicationContext가 bean들을 다 관리해준다. 스프링 컨테이너에 객체 생성한 것들을 다 집어넣고 관리해준다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- inflearn-spring-core-principles/src/main/java/com/inflearn/order/AppConfig.java
+++ inflearn-spring-core-principles/src/main/java/com/inflearn/order/AppConfig.java
@@ -1,27 +1,35 @@
 package com.inflearn.order;
 
 import com.inflearn.discount.DiscountPolicy;
-import com.inflearn.discount.FixDiscountPolicy;
+import com.inflearn.discount.RateDiscountPolicy;
 import com.inflearn.member.MemberRepository;
 import com.inflearn.member.MemberService;
 import com.inflearn.member.MemberServiceimpl;
 import com.inflearn.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 설정정보, 애플리케이션이 어떻게 구성되는지 구성정보를 담당하는 설정정보
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceimpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceimpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
