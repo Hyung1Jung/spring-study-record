2645bef8568e1b8a6dec01c1503697078f663726
Hyung1Jung <junghyungile@gmail.com>
2021-01-28 15:11:01 +0900

    feat : 빈 이름, 이름없이 타입, 구체 타입으로 조회 테스트코드

---
+++ inflearn-spring-core-principles/src/test/java/com/inflearn/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,50 @@
+package com.inflearn.beanfind;
+
+import com.inflearn.member.MemberService;
+import com.inflearn.member.MemberServiceimpl;
+import com.inflearn.order.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceimpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceimpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceimpl.class); // 역할에 의존해야하는데 구현에 의존한 테스트리 좋은 경우는 아니다.
+        assertThat(memberService).isInstanceOf(MemberServiceimpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+//  ac.getBean("xxxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class)); // assertThrows -> 자바 8의 람다기능을 쓴다.
+
+    }
+
+
+}
