03e468c5bd0eacc15beb33be593c2160bf4fe4bf
Hyung1Jung <junghyungile@gmail.com>
2021-02-01 03:27:31 +0900

    feat : 싱글톤 적용

---
+++ inflearn-spring-core-principles/src/test/java/com/inflearn/singleton/SingletonTest.java
@@ -0,0 +1,39 @@
+package com.inflearn.singleton;
+
+import com.inflearn.member.MemberService;
+import com.inflearn.order.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+    public static void main(String[] args) {
+
+        AppConfig appConfig = new AppConfig();
+
+        // 1.조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        // 2. 조회 : 호출할 때 마다 객체를 생성성
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조 값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+
+    }
+}
