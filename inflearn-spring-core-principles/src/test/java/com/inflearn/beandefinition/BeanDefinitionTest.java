e22bb4a9537aeaca9a2f471348ad6b411616b8cb
Hyung1Jung <junghyungile@gmail.com>
2021-01-31 20:57:42 +0900

    feat : 스프링 빈 설정 메타 정보 - BeanDefinition

---
+++ inflearn-spring-core-principles/src/test/java/com/inflearn/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,28 @@
+package com.inflearn.beandefinition;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BeanDefinitionTest {
+
+    //    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + "beanDefinition = " + beanDefinition);
+            }
+        }
+
+    }
+
+}
