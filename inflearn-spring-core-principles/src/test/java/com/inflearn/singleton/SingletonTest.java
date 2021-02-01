d4323497dfe45b533e123f0e6957ed6c3bee214b
Hyung1Jung <junghyungile@gmail.com>
2021-02-01 13:33:06 +0900

    refactor : 스프링 컨테이너와 싱글톤

--- inflearn-spring-core-principles/src/test/java/com/inflearn/singleton/SingletonTest.java
+++ inflearn-spring-core-principles/src/test/java/com/inflearn/singleton/SingletonTest.java
@@ -2,15 +2,18 @@ package com.inflearn.singleton;
 
 import com.inflearn.member.MemberService;
 import com.inflearn.order.AppConfig;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingletonTest {
-    public static void main(String[] args) {
 
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
         AppConfig appConfig = new AppConfig();
 
         // 1.조회 : 호출할 때 마다 객체를 생성
@@ -35,5 +38,23 @@ public class SingletonTest {
 
         assertThat(singletonService1).isSameAs(singletonService2);
 
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+
+        // 참조 값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
