1d4d4b53ae5321217bdb1d45856ca7e43064bd16
Hyung1Jung <junghyungile@gmail.com>
2021-02-01 14:07:20 +0900

    refactor : 싱글톤 방식의 주의점, 필드는 무상태로 설계하자

---
+++ inflearn-spring-core-principles/src/test/java/com/inflearn/singleton/StatefulServiceTest.java
@@ -0,0 +1,38 @@
+package com.inflearn.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA : A 사용자 10000원 주문
+        int userAPrice = statefulService1.order("userA", 10000);
+        // ThreadB : B 사용자 20000원 주문
+        int userBPrice = statefulService2.order("userB", 20000);
+
+        //ThreadA : 사용자 A 주문 금액 조회
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
+
+//        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
