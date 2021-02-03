711c835241bd7d25930fd9cef16e4972ceb50e3b
Hyung1Jung <junghyungile@gmail.com>
2021-02-03 13:16:30 +0900

    feat : 중복과 등록과 충돌

--- inflearn-spring-core-principles/src/main/java/com/inflearn/order/AutoAppConfig.java
+++ inflearn-spring-core-principles/src/main/java/com/inflearn/order/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package com.inflearn.order;
 
+import com.inflearn.member.MemberRepository;
+import com.inflearn.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,4 +13,9 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) // @Configuration 내부를 보면 @Component가 있다. 그래도 자동으로 CompoentScan에 대상이 되기 때문에 제외시켜준다.
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
