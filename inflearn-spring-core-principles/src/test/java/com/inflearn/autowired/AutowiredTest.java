7760120c2ad847a0bcadd4d8b23a2f976c953c46
Hyung1Jung <junghyungile@gmail.com>
2021-02-03 15:42:54 +0900

    feat : 옵션처리

---
+++ inflearn-spring-core-principles/src/test/java/com/inflearn/autowired/AutowiredTest.java
@@ -0,0 +1,38 @@
+package com.inflearn.autowired;
+
+import com.inflearn.member.Member;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+
+        @Autowired(required = false)
+        public void setNoBean1(Number noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+
+    }
+}
