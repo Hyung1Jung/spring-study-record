f3e468d695ceea1f13cbe4f66e3da0a8d1abc5fb
Hyung1Jung <junghyungile@gmail.com>
2021-02-05 15:44:07 +0900

    feat() : 빈 등록 초기화 메서드

--- inflearn-spring-core-principles/src/test/java/com/inflearn/lifecycle/BeanLifeCycleTest.java
+++ inflearn-spring-core-principles/src/test/java/com/inflearn/lifecycle/BeanLifeCycleTest.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class BeanLifeCycleTest {
 
     @Test
-    public void lifeCycleTest() {
+    public void flifeCycleTest() {
         ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
         NetworkClient client = ac.getBean(NetworkClient.class);
         ac.close();
@@ -18,7 +18,7 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean
+        @Bean(initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
@@ -27,3 +27,4 @@ public class BeanLifeCycleTest {
 
     }
 }
+
--- inflearn-spring-core-principles/src/test/java/com/inflearn/lifecycle/NetworkClient.java
+++ inflearn-spring-core-principles/src/test/java/com/inflearn/lifecycle/NetworkClient.java
@@ -1,9 +1,7 @@
 package com.inflearn.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -29,14 +27,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close : " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() throws Exception {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
