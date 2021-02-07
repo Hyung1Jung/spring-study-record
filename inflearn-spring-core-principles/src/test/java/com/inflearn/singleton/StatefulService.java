be35fe8a9a1766be3d25b70603711b3d8f743c34
Hyung1Jung <junghyungile@gmail.com>
2021-02-07 17:11:09 +0900

    feat : request scope

--- inflearn-spring-core-principles/src/test/java/com/inflearn/singleton/StatefulService.java
+++ inflearn-spring-core-principles/src/test/java/com/inflearn/singleton/StatefulService.java
@@ -1,9 +1,9 @@
 package com.inflearn.singleton;
 
 public class StatefulService {
-    
+
 //    private int price; // 상태를 유지하는 필드
-    
+
     public int order(String name, int price) {
         System.out.println("name = " + name + ", price = " + price);
 //        this.price = price; // 여기가 문제
