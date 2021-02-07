ca3797a189890724314b80e8d90a0487b8c987f1
Hyung1Jung <junghyungile@gmail.com>
2021-02-07 17:11:03 +0900

    feat : request scope

---
+++ inflearn-spring-core-principles/src/main/java/com/inflearn/web/LogDemoController.java
@@ -0,0 +1,31 @@
+package com.inflearn.web;
+
+import com.inflearn.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURI().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "ok";
+
+    }
+
+}
---
+++ inflearn-spring-core-principles/src/main/java/com/inflearn/web/LogDemoService.java
@@ -0,0 +1,16 @@
+package com.inflearn.web;
+
+import com.inflearn.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
