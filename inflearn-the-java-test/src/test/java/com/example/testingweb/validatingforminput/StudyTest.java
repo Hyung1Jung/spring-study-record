9aa4c8d85eda3909dfdf51688f8a1973aef68474
Hyung1Jung <junghyungile@gmail.com>
2021-03-15 19:56:32 +0900

    feat : assertThat

--- inflearn-the-java-test/src/test/java/com/example/testingweb/validatingforminput/StudyTest.java
+++ inflearn-the-java-test/src/test/java/com/example/testingweb/validatingforminput/StudyTest.java
@@ -2,29 +2,17 @@ package com.example.testingweb.validatingforminput;
 
 import org.junit.jupiter.api.*;
 
-import java.time.Duration;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
-/*
-    100이 넘으면 300까지 기다릴 필요 없이 바로 종료, assertTimeoutPreemptively
-
-    주의해서 사용하여함. 테스트코드에서 이 코드블럭은 별도의 쓰레드에서 실행하기 때문에
-    만약에 ThreadLocal을 사용하는 코드블럭이 있다면, 예상치 못한 에러가 발생할 수 있다.
-    스프링 트랜잭션 전략 같은 경우는 ThreadLocal을 기본적으로 사용을 하는데 ThreadLocal는 다른 Thread는 공유가 안되기 때문에 스프링이 만든 트랜잭션 설정이 적용이 제대로 안될 수 가 있다.
-    트랜잭션은 롤백을 기본적으로 실행을 하는데 롤백이 안되고 DB에 반영이 되는 경우가 생길 수 있다. 트랜잭션 설정을 가지고 있는 쓰레드와 별개로 실항하기 때문이다. 쓰레드와 관련이 없는 코드를 실행할 경우 괞찮다.
-*/
 
     @Test
     @DisplayName("스터디 만들기")
     void create_new_Study() {
-        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
-            new Study(10);
-            Thread.sleep(300);
-        });
+        Study actual = new Study(10);
+        assertThat(actual.getLimit()).isGreaterThan(0);
     }
 
     @Test
