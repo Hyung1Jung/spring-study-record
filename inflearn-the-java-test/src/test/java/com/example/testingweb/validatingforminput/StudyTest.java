26c1549d18844132fc846b7f46bcf00cb93043e3
Hyung1Jung <junghyungile@gmail.com>
2021-03-16 19:36:12 +0900

    feat : 테스트 반복하기(1)

--- inflearn-the-java-test/src/test/java/com/example/testingweb/validatingforminput/StudyTest.java
+++ inflearn-the-java-test/src/test/java/com/example/testingweb/validatingforminput/StudyTest.java
@@ -1,7 +1,10 @@
 package com.example.testingweb.validatingforminput;
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.lang.annotation.Retention;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -13,8 +16,24 @@ class StudyTest {
     void create_new_Study() {
         Study actual = new Study(100);
         assertThat(actual.getLimit()).isGreaterThan(0);
-
     }
+
+    // 아주 간단하게 테스트를 반복할 수 있다. 몇 번을 반복할지 적어주고, 테스트를 작성하면 된다.
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" +
+                repetitionInfo.getTotalRepetitions());
+    }
+
+    // 반복적인 테스트를 할 때마다, 다른 값들을 가지고 테스트틀 하고싶다.
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
+    void parameterizedTest(String message) {
+        System.out.println(message);
+    }
+
 
     @SlowTest
     @DisplayName("스터디 만들기 slow")
