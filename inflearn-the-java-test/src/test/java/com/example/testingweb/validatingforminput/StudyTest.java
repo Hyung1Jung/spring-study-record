a6555003172d9c20f01aaa8a2fec7575abf27d50
Hyung1Jung <junghyungile@gmail.com>
2021-03-17 18:33:28 +0900

    feat : SimpleAfgumentConverter을 상속받아 @ValueSource이용

--- inflearn-the-java-test/src/test/java/com/example/testingweb/validatingforminput/StudyTest.java
+++ inflearn-the-java-test/src/test/java/com/example/testingweb/validatingforminput/StudyTest.java
@@ -2,11 +2,14 @@ package com.example.testingweb.validatingforminput;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.lang.annotation.Retention;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
@@ -29,11 +32,18 @@ class StudyTest {
     // 반복적인 테스트를 할 때마다, 다른 값들을 가지고 테스트틀 하고싶다.
     @DisplayName("스터디 만들기")
     @ParameterizedTest(name = "{index} {displayName} message = {0}")
-    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
-    void parameterizedTest(String message) {
-        System.out.println(message);
+    @ValueSource(ints = {10, 20, 40})
+    void parameterizedTest(@ConvertWith(StudyConverter.class) Study study) {
+        System.out.println(study.getLimit());
     }
 
+    static class StudyConverter extends SimpleArgumentConverter{
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
+    }
 
     @SlowTest
     @DisplayName("스터디 만들기 slow")
